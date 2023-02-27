fdd96288e80f2b870f496e9c904bc74d7629d462
Kanagaraj.M <112673484+KANAGARAJ-M@users.noreply.github.com>
2023-02-27 21:58:33 +0530

    edited

---
+++ lib/privacypolicy.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class NewMemory extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+          child: Container(
+              height: 300,
+              width: 300,
+              child: Image.asset('assets/Translator.png'))),
+    );
+  }
+}
